b49a77bab534ba05e94dcde0ab73bfde54570ac8
Abhinav Chauhan <abhinav@Abhinavs-MacBook-Air.local>
2025-01-05 17:04:50 +0530

    minor changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Kofi-clone/", // Add this line to set the base path for GitHub Pages
   plugins: [react()],
-})
+});
